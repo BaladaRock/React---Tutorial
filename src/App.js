86ee8f85d0899b36958432b4bb11e2391fc16993
Andrei Daian <andreidaian05@gmail.com>
2021-01-31 15:20:10 +0200

    Finished props and stats. Each newly created post appears in the TweetList component. Added delete button by filtering tweets after an ID

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
+import TweetList from "./components/TweetList";
+import CreateTweet from "./components/CreateTweet";
 
 function App() {
+  // stats
+  const [name, setName] = useState("Andrei");
+  const [textInput, setTextInput] = useState("");
+  const [tweets, setTweets] = useState([]);
+
+  const tweetsContent = "R2 B2 R F R'B2 R F' R";
+
   return (
-    <div>
-     <h1>Hi, Andrei!</h1>
+    <div className="tweet">
+      <h1>Hi, Andrei! This is your first app in React</h1>
+      <CreateTweet
+        textInput={textInput}
+        setTextInput={setTextInput}
+        tweets={tweets}
+        setTweets={setTweets}
+      />
+      <TweetList name={name} tweets={tweets} setTweets={setTweets} />
     </div>
   );
 }
---
+++ src/components/CreateTweet.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const CreateTweet = ({ textInput, setTextInput, tweets, setTweets }) => {
+  const defaultHeader = "This reflects your newly created tweet content";
+  const userInputHandler = event => {
+    setTextInput(event.target.value);
+  };
+  const submitTweetHandler = event => {
+    event.preventDefault();
+    setTweets([...tweets, { message: textInput, id: uuidv4() }]);
+    setTextInput("");
+  };
+
+  return (
+    <form onSubmit={submitTweetHandler}>
+      <textarea
+        value={textInput}
+        onChange={userInputHandler}
+        cols="50"
+        rows="5"
+      ></textarea>
+      <button>Submit</button>
+      <h3 onClick={() => setTextInput("")}>
+        {!textInput ? defaultHeader : textInput}
+      </h3>
+    </form>
+  );
+};
+
+export default CreateTweet;
